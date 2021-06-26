2a9cfb887a02393b525b28d8afccc5568d2f3bff
davidGarcia <djgj57@hotmail.com>
2021-06-25 19:29:57 -0500

    Actualiza ingreso de tarea de html a text - seguridad

--- scripts/todos.js
+++ scripts/todos.js
@@ -26,7 +26,7 @@ window.addEventListener('load', () => {
         `<li class="tarea">
             <div class="not-done"></div>
             <div class="descripcion">
-               <p class="nombre">${toDo.description}</p>
+               <p class="nombre"></p>
                 <p class="timestamp">Creada: ${toDo.createdAt}</p>
             </div>
          </li>`
@@ -38,6 +38,7 @@ window.addEventListener('load', () => {
     function renderizarTodos() {
         listadoTodos.forEach(toDo => {
             tareasPendientes.innerHTML += nuevoToDo(toDo)
+            document.querySelectorAll('.tareas-pendientes .nombre')[document.querySelectorAll('.tareas-pendientes .nombre').length-1].innerText = toDo.description;
         });
     }
 
@@ -68,13 +69,16 @@ window.addEventListener('load', () => {
         return formato.replace(/dd|mm|yy|yyy/gi, matched => map[matched])
     }
    
+    //Crea la tarea si esta no esta en blanco
     document.querySelector('button').addEventListener("click", function (event) {
         let tarea = document.getElementById("iTarea").value;
         if (tarea.length > 0) {
             let hoy = new Date();
             tareaAux = crearToDo(tarea, formatoFecha(hoy, 'dd/mm/yy'));
+            //Se agrega para tener todos los toDo.
             listadoTodos.push(tarea);
             tareasPendientes.innerHTML += nuevoToDo(tareaAux);
+            document.querySelectorAll('.tareas-pendientes .nombre')[document.querySelectorAll('.tareas-pendientes .nombre').length-1].innerText = tarea
         }
         document.getElementById("iTarea").value = "";
         event.preventDefault();
