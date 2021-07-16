cca01ceef20d5378040484adc5f77e37b2a0f599
davidGarcia <djgj57@hotmail.com>
2021-07-15 23:34:20 -0500

    solo falta funcionalidad de eliminar

--- scripts/todos.js
+++ scripts/todos.js
@@ -17,19 +17,22 @@ window.addEventListener('load', () => {
     })
 
     //Todos los toDo - Se piden todas las tareas a la API https://ctd-todo-api.herokuapp.com/
-    const urlGettodos  = 'https://ctd-todo-api.herokuapp.com/v1/tasks';
+    const urlGettodos = 'https://ctd-todo-api.herokuapp.com/v1/tasks';
 
-    const setting = {
+    const settingTraerTareas = {
         method: 'GET',
         headers: {
             "Authorization": JSON.parse(sessionStorage.getItem('todosUsuario'))
         }
     };
-    
-    fetch(urlGettodos, setting)
-    .then((response) => response.json())
-    .then((json) =>   dibujarTodo(json))
-    .catch((e) => console.log(e))
+
+    function traerTareas() {
+        fetch(urlGettodos, settingTraerTareas)
+            .then((response) => response.json())
+            .then((json) => dibujarTodos(json))
+            .catch((e) => console.log(e))
+    }
+    traerTareas()
 
     //template de un toDo en una funcion
     let nuevoToDo = toDo =>
@@ -42,17 +45,20 @@ window.addEventListener('load', () => {
          </li>`
 
     // Renderizar totas las tareas obtenidas
-    function dibujarTodo(arreglo){
-    // Se filtran completas e incompletas
-    let arrayTareasPendientes = arreglo.filter(x => x.completed == false)
-    let arrayTareasterminadas = arreglo.filter(x => x.completed == true)
-    renderizarTodos(arrayTareasPendientes)
-    renderizarTodosFinalizados(arrayTareasterminadas)
+    function dibujarTodos(arreglo) {
+        // Se filtran completas e incompletas
+        let arrayTareasPendientes = arreglo.filter(x => x.completed == false)
+        let arrayTareasterminadas = arreglo.filter(x => x.completed == true)
+        renderizarTodos(arrayTareasPendientes)
+        renderizarTodosFinalizados(arrayTareasterminadas)
+        retornarTareas(document.querySelectorAll('.tarea'))
     }
+
 
     const tareasPendientes = document.querySelector('.tareas-pendientes');
 
     function renderizarTodos(arregloFiltrado) {
+        tareasPendientes.innerHTML = ""
         arregloFiltrado.forEach(toDo => {
             tareasPendientes.innerHTML += nuevoToDo(toDo)
             document.querySelectorAll('.tareas-pendientes .nombre')[document.querySelectorAll('.tareas-pendientes .nombre').length - 1].innerText = toDo.description;
@@ -62,53 +68,45 @@ window.addEventListener('load', () => {
     const tareasFinalizadas = document.querySelector('.tareas-terminadas');
 
     function renderizarTodosFinalizados(arregloFiltrado) {
+        tareasFinalizadas.innerHTML = ""
         arregloFiltrado.forEach(toDo => {
             tareasFinalizadas.innerHTML += nuevoToDo(toDo)
             document.querySelectorAll('.tareas-terminadas .nombre')[document.querySelectorAll('.tareas-terminadas .nombre').length - 1].innerText = toDo.description;
         });
     }
 
-  
-
     //crear nuevo toDo
 
-    function crearToDo(descripcion) {
+    function crearToDo(descripcion, estado) {
         return {
             description: descripcion,
-            completed: false
+            completed: estado
         }
     }
-
-    //    Para obtener fecha
-
-    // function fechaDeHoy() {
-    //     let date = new Date()
-
-    //     let day = date.getDate().toString()
-    //     let month = date.getMonth() + 1
-    //     let year = date.getFullYear().toString().slice(-2)
-
-    //     if (month < 10 && day < 10) {
-    //         return `0${day}/0${month}/${year}`
-    //     } else if (month < 10) {
-    //         return `${day}/0${month}/${year}`
-    //     } else if (day < 10) {
-    //         return `0${day}/${month}/${year}`
-    //     } else {
-    //         return `${day}/${month}/${year}`
-    //     }
-    // }
 
     //Crea la tarea si esta no esta en blanco
     document.querySelector('button').addEventListener("click", function (event) {
         let tarea = document.getElementById("iTarea").value;
         if (tarea.length > 0) {
-            tareaAux = crearToDo(tarea);
+            tareaAux = crearToDo(tarea, false);
             //Se debe crear la tarea en la API https://ctd-todo-api.herokuapp.com/
-            listadoTodos.push(tarea);
 
-            // tareasPendientes.innerHTML += nuevoToDo(tareaAux);
-            // document.querySelectorAll('.tareas-pendientes .nombre')[document.querySelectorAll('.tareas-pendientes .nombre').length - 1].innerText = tarea
+            const urlCrearTarea = 'https://ctd-todo-api.herokuapp.com/v1/tasks';
+
+            const settingCrearTarea = {
+                method: 'POST',
+                headers: {
+                    "Content-type": 'application/json',
+                    "Authorization": JSON.parse(sessionStorage.getItem('todosUsuario'))
+                },
+                body: JSON.stringify(tareaAux)
+            };
+
+            fetch(urlCrearTarea, settingCrearTarea)
+                .then((response) => response.json())
+                .then((json) => traerTareas())
+                .catch((e) => console.log(e))
+
         }
         document.getElementById("iTarea").value = "";
         event.preventDefault();
@@ -116,6 +114,55 @@ window.addEventListener('load', () => {
 
     //Actualizar tarea de pendiente a terminada
 
+    function retornarTareas(tareas) {
+        console.log(tareas);
+        for (let i = 0; i < tareas.length; i++) {
+            tareas[i].addEventListener('click', function () {
+                let id = this.id;
+                let message = this.childNodes[3].childNodes[1].textContent;
+                actualizar(message, id, true)
+            })
+        }
+    }
+
+    function actualizar(mensaje, id, estado) {
+
+        tareaAuxActualizar = crearToDo(mensaje, estado);
+
+        const urlActualizarTarea = `https://ctd-todo-api.herokuapp.com/v1/tasks/${id}`;
+
+        const settingActualizarTarea = {
+            method: 'PUT',
+            headers: {
+                "Content-type": 'application/json',
+                "Authorization": JSON.parse(sessionStorage.getItem('todosUsuario'))
+            },
+            body: JSON.stringify(tareaAuxActualizar)
+        };
+
+        fetch(urlActualizarTarea, settingActualizarTarea)
+            .then((response) => response.json())
+            .then((json) => traerTareas())
+            .catch((e) => console.log(e))
+    }
+
     //Eliminar tarea
+    function eliminar(id) {
+
+        const urlEliminarTarea = `https://ctd-todo-api.herokuapp.com/v1/tasks/${id}`;
+
+        const settingEliminarTarea = {
+            method: 'DELETE',
+            headers: {
+                "Content-type": 'application/json',
+                "Authorization": JSON.parse(sessionStorage.getItem('todosUsuario'))
+            }
+        };
+
+        fetch(urlEliminarTarea, settingEliminarTarea)
+            .then((response) => response.json())
+            .then((json) => traerTareas())
+            .catch((e) => console.log(e))
+    }
 
 })
